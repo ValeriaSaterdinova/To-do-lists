8e01bf968de2b8361fb5099db07b2ebd0702b5d8
Valeria <vsaterdinova.exceedteam@gmail.com>
2021-11-09 11:36:33 +0300

    bugs fixed

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-let allTasks = JSON.parse(localStorage.getItem('tasks'))|| [];
+let allTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 let valueInput = '';
 let input = null;
 
-window.onload = function init() {
+window.onload = init = () => {
   input = document.getElementById('add-task');
-  input.addEventListener('change', updateValue)
+  input.addEventListener('change', updateValue);
   localStorage.setItem('tasks', JSON.stringify(allTasks));
   render();
-}
-
-onClickButton = () => {
+} 
+const onClickButton = () => {
   allTasks.push({
     text: valueInput,
-    isCheck: false  
-  }); 
+    isCheck: false
+  });
   localStorage.setItem('tasks', JSON.stringify(allTasks));
   valueInput = '';
   input.value = '';
-  
   render();
 }
 
-updateValue = (event) => {
+const updateValue = (event) => {
   valueInput = event.target.value;
 }
-render = () => {
+
+const render = () => {
   const content = document.getElementById('content-page');
   while (content.firstChild) {
     content.removeChild(content.firstChild);
   }
   allTasks.sort((a, b) => a.isCheck - b.isCheck);
-
-
   allTasks.map((item, index) => {
-    const container = document.createElement('div');
+    let {text, isCheck} = item;
+    const container = document.createElement('div')
     container.id = `task-${index}`;
-    container.className = 'task-container'
+    container.className = 'task-container';
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = item.isCheck;
-    checkbox.onchange = function () {
+    checkbox.onchange = () => {
       onChangeCheckBox(index);
     };
     container.appendChild(checkbox);
-    const text = document.createElement('p');
-    text.innerText = item.text;
-    text.className = item.isCheck ? 'text-task done-text' : 'text-task';
-    container.appendChild(text);
-
+    const text1 = document.createElement('p');
+    text1.innerText = text;
+    text1.className = isCheck ? 'text-task done-text' : 'text-task';
+    container.appendChild(text1);
     const imageEdit = document.createElement('img');
     imageEdit.src = "Редактирование.svg";
     container.appendChild(imageEdit);
     allTasks.forEach(event => {
-    if (text.className === 'text-task') {
-    imageEdit.onclick = () => {
-      const inputTask = document.createElement('input');
-      inputTask.type = 'text';
-      inputTask.value = text.innerText;
-      container.replaceChild(inputTask, text);
-      imageEdit.onclick = () => {
-        item.text = inputTask.value;
-        localStorage.setItem('tasks', JSON.stringify(allTasks));
-        render();
+      if (text1.className === 'text-task') {
+        imageEdit.onclick = () => {
+          const inputTask = document.createElement('input');
+          inputTask.type = 'text';
+          inputTask.value = text1.innerText;
+          container.replaceChild(inputTask, text);
+          imageEdit.onclick = () => {
+            text = inputTask.value;
+            localStorage.setItem('tasks', JSON.stringify(allTasks));
+            render();
+          }
+          imageDelete.onclick = () => {
+            render();
+          }
+        }
       }
-      imageDelete.onclick = () => {
-        render();
-    }
-  }
-}
-});
+    });
+
     const imageDelete = document.createElement('img');
     imageDelete.src = "delete.svg";
     container.appendChild(imageDelete);
@@ -78,25 +76,27 @@ render = () => {
     content.appendChild(container);
   });
 }
+
 const deleteValue = (index) => {
-  allTasks  = allTasks.filter((item, index1) => (index1 !== index));
+  allTasks = allTasks.filter((item, index1) => (index1 !== index));
   localStorage.setItem('tasks', JSON.stringify(allTasks));
   render();
 };
 
-onChangeCheckBox = (index) => {
+const onChangeCheckBox = (index) => {
   allTasks[index].isCheck = !allTasks[index].isCheck;
   localStorage.setItem('tasks', JSON.stringify(allTasks));
   render();
 }
- 
-onClickButtonClear = () => {
+
+const onClickButtonDelete = () => {
+  window.localStorage.removeItem('tasks');
+  allTasks = [];
+  render();
+}
+
+const onClickButtonClear = () => {
   window.localStorage.clear();
-  location.reload();
+  allTasks = [];
+  render();
 }
-
-onClickButtonDelete = () => {
-  window.localStorage.removeItem('tasks')
-  location.reload();
-}
-
